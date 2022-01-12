9b970eb67abb93f0b3674d0977baa09e715093c2
ccmywish <ccmywish@qq.com>
2022-01-12 15:55:47 +0800

    add help

---
+++ cr.cs
@@ -0,0 +1,33 @@
+﻿//   ---------------------------------------------------
+//   File          : cr.cs
+//   Authors       : ccmywish <ccmywish@qq.com>
+//   Created on    : <2021-1-12>
+//   Last modified : <2022-1-12>
+//
+//   This file is used to explain a CRyptic command
+//   or an acronym's real meaning in computer world or
+//   orther fileds.
+//
+//  ---------------------------------------------------
+
+const string CRYPTIC_VERSION = "1.0.0";
+
+
+void help() 
+{
+    string help = @"cr: Cryptic Resolver version {0} in C#
+
+usage:
+    cr -h                     => print this help
+    cr -u (xx.com//repo.git)  => update default sheet or add sheet from a git repo
+    cr emacs                  => Edit macros: a feature-rich editor";
+	
+    Console.WriteLine(help, CRYPTIC_VERSION);
+}
+
+help();
+
+
+// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
