d82769fb1162605beab3a3abac1afe333a79cc30
ccmywish <ccmywish@qq.com>
2022-01-12 16:17:41 +0800

    add main logic

--- cr.cs
+++ cr.cs
@@ -13,6 +13,22 @@
 const string CRYPTIC_VERSION = "1.0.0";
 
 
+
+
+void add_default_sheet_if_none_exist(){
+    Console.WriteLine("TODO: add default sheet");
+}
+
+
+void update_sheets(string sheet_repo){
+    Console.WriteLine("TODO: update sheets");
+}
+
+void solve_word(string word_2_solve){
+    Console.WriteLine("TODO: solve word");
+}
+
+
 void help() 
 {
     string help = @"cr: Cryptic Resolver version {0} in C#
@@ -25,9 +41,41 @@ usage:
     Console.WriteLine(help, CRYPTIC_VERSION);
 }
 
-help();
 
 
-// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+// no need to write
+// static void Main(string args)
+// and args are already in this global scope
+
+string arg;	
+int arg_num = args.Length;	// can't be args.length
+
+if(arg_num < 1) {
+	arg = "";
+} else {
+	arg = args[0];
+}
+
+// Console.WriteLine($"{arg_num} {arg}"); // DEBUG
+
+switch (arg) {
+case "":
+	help();
+	add_default_sheet_if_none_exist();
+	break;
+case "-h":
+	help();
+	break;
+case "-u":
+	if (arg_num > 1) {
+		update_sheets(args[1]);
+	} else {
+		update_sheets("");
+	}
+	break;
+default:
+	solve_word(arg);
+    break;  // need this to make compiler happy
+}
+
 
